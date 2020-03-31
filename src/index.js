6bb5a86775f15bfb62c0b7854fcfaa57bd0d3c4b
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-03-31 14:13:42 -0300

    :rocket: Adiciona s3 filestorage
    Finaliza backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -10,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+	'/files',
+	express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 app.use(routes);
 
 app.listen(3000);
